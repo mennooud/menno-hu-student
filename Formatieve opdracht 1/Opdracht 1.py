4a8fc5f2c856891030605fcef46f10f60a18b1f9
mennooud <60653415+mennooud@users.noreply.github.com>
2020-02-07 19:29:16 +0100

    opdr3
    Opdracht 3 volledig

--- Formatieve opdracht 1/Opdracht 1.py
+++ Formatieve opdracht 1/Opdracht 1.py
@@ -1,3 +1,10 @@
+""""
+Opdracht 1 - PyramidePyramide.png
+Schrijf een programma dat een piramide van variabele lengte afdrukt, zoals in het voorbeeld. 
+Druk ieder character apart af. De gebruiker geeft aan hoe groot de piramide
+is. Implementeer je programma twee keer, de eerste keer met twee for loops, en daarna met twee while loops. Maak ook versies die de pyramide een andere kant op laten wijzen.
+""""
+
 x = int(input("Hoe groot: "))
 
 for i in range(x):
--- Formatieve opdracht 1/Opdracht 3.py
+++ Formatieve opdracht 1/Opdracht 3.py
@@ -1,1 +1,43 @@
-def count():
+
+# a. Schrijf een functie count() die berekent hoe vaak een geheel getal x in een lijst voorkomt.
+print("\nOpdracht 3.a")
+getallenlijst = 1,2,3,4,5,6,7,7,7,8,8,3,3,5,65,7,8,9,90,0
+
+def count(lijst):
+    invoer = int(input("Voer geheel getal in: "))
+    ratio = 0
+    for i in lijst:
+        if invoer == i:
+            ratio += 1
+    print("Getal "+ str(invoer) +" komt "+ str(ratio) + " keer voor")
+count(getallenlijst)
+
+# b. Schrijf een functie die in een gegeven lijst het grootste verschil tussen twee op een volgende getallen bepaalt.
+print("\nOpdracht 3.b")
+lijst2 = [45,65,4,4,3,4,5,6,4,5,7,4,5,6,257,6]
+
+def GrootsteVerschil():
+    print(max([y-x for x, y in zip(lijst2[:-1], lijst2[1:])]))
+
+GrootsteVerschil()
+
+# c.  Schrijf een functie, die bepaalt of een gegeven lijst met alleen 1’en en 0’en aan de volgende eisen voldoet:
+print("\nOpdracht 3.c")
+lijst3 = 1,1,1,0,0,1,0,1,0,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,1,0
+
+def Voldoet():
+    nullen = lijst3.count(0)
+    enen = lijst3.count(1)
+    if enen > nullen:
+        print("Het aantal enen("+str(enen)+ ") is groter dan het aantal nullen("+str(nullen)+ ")")
+        print(True)
+        return True
+    if enen < nullen:
+        print("Het aantal enen("+str(enen)+ ") is kleiner dan het aantal nullen("+str(nullen)+ ")")
+        print(False)
+        return False
+    if nullen > 12:
+        print("Er zijn te veel nullen("+str(nullen)+ ")")
+        print(False)
+        return False
+Voldoet()
