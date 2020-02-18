1a0f5565ca1ba6259ffbf76590b60ded70497b70
EmptySpot <47001654+EmptySpot@users.noreply.github.com>
2020-02-18 13:51:05 +0100

    Tip voor database

--- Summatieve Opdrachten/Mastering_Mastermind/Game/Database.py
+++ Summatieve Opdrachten/Mastering_Mastermind/Game/Database.py
@@ -9,3 +9,26 @@ def Database():
     all_answers = set(itertools.permutations("+-xo*?", 4))
     return all_answers
 
+
+data = Database()
+print(data)
+print(len(data))
+
+def VoorbeeldVanDatabase():
+    tekendictonairy = {'1' :'+', '2' : '-', '3' : "x", '4' :  "o", '5' : "*", '6' : "?"}
+    all_answers = []
+    for i in range(1111, 6667):
+        answer = str(i)
+        if '7' in answer or '8' in answer or '9' in answer or '0' in answer:
+            continue
+        else:
+            a = ((tekendictonairy[answer[0]]))      #Dit is niet heel clean door mij gemaakt, maar errors zorgde hiervoor
+            b = ((tekendictonairy[answer[1]]))
+            c = ((tekendictonairy[answer[2]]))
+            d = ((tekendictonairy[answer[3]]))
+            all_answers.append(a+b+c+d)
+    print(all_answers)
+    print(len(all_answers))
+    return all_answers
+
+VoorbeeldVanDatabase()
