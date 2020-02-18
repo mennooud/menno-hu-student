9c5932207937a3fe2e626b0adfa19e52e67ba00c
mennooud <60653415+mennooud@users.noreply.github.com>
2020-02-18 13:06:34 +0100

    18-02-2020
    Game werkt

---
+++ Summatieve Opdrachten/Mastering_Mastermind/Game/Database.py
@@ -0,0 +1,11 @@
+import random
+import itertools
+import collections
+import sys
+
+
+def Database():
+    Symbols = "+-xo*?"
+    all_answers = set(itertools.permutations("+-xo*?", 4))
+    return all_answers
+
--- Summatieve Opdrachten/Mastering_Mastermind/Mastermind_GUI.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300) #win.setGeometry(xpos, ypos, width, height)
